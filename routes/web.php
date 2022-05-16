a5df8cc2046b4062731bbcca7be0dd1b63de4508
Manul Wickramanayaka <mwickraman@wiley.com>
2022-05-16 12:02:06 +0530

    admin dashboard aded

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+class App extends Illuminate\Support\Facades\App
+{
+}
+class Artisan extends Illuminate\Support\Facades\Artisan
+{
+}
+class Auth extends Illuminate\Support\Facades\Auth
+{
+}
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +24,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [App\Http\Controllers\Admin\dashboardController::class, 'index']);
 });
